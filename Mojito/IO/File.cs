e348e7fe1879978e026b387eae871d32d0a8d93e
Jerry Wang <freebufer.wang@gmail.com>
2023-02-21 19:56:20 +0800

    feat: Add IO.File class

---
+++ Mojito/IO/File.cs
@@ -0,0 +1,43 @@
+﻿namespace Mojito.IO;
+
+public static class File
+{
+    /// <summary>
+    /// Create file
+    /// </summary>
+    /// <param name="path">File path</param>
+    /// <returns></returns>
+    public static Result Create(string path)
+    {
+        return Create(path, FileMode.CreateNew);
+    }
+
+    /// <summary>
+    /// Create the file with FileMode
+    /// </summary>
+    /// <param name="path">File path</param>
+    /// <param name="fileMode">File mode</param>
+    /// <returns></returns>
+    public static Result Create(string path, FileMode fileMode)
+    {
+        try
+        {
+            using var fs = new FileStream(path, fileMode);
+            return Result.Ok;
+        }
+        catch (Exception ex)
+        {
+            return Result.Error(ex);
+        }
+    }
+
+    /// <summary>
+    /// Check whether the file exists
+    /// </summary>
+    /// <param name="path">File path</param>
+    /// <returns></returns>
+    public static bool Exists(string path)
+    {
+        return System.IO.File.Exists(path);
+    }
+}
