eac6253ba4ae092f4a3334ba20f39969c6466565
Wjinlei <freebufer.wang@gmail.com>
2023-02-24 14:27:41 +0800

    test: Add StringTest class.

---
+++ TestMojito/Random/StringTest.cs
@@ -0,0 +1,44 @@
+﻿namespace TestMojito.Random;
+
+public class StringTest
+{
+    [Test]
+    public void TestLetters()
+    {
+        var randomAlpha = Mojito.Random.String.Letters(10);
+        TestContext.Out.WriteLine(randomAlpha);
+        Assert.That(randomAlpha, Has.Length.EqualTo(10));
+    }
+
+    [Test]
+    public void TestNumbers()
+    {
+        var randomAlpha = Mojito.Random.String.Numbers(10);
+        TestContext.Out.WriteLine(randomAlpha);
+        Assert.That(randomAlpha, Has.Length.EqualTo(10));
+    }
+
+    [Test]
+    public void TestCombinationOfLettersAndNumbers()
+    {
+        var randomAlpha = Mojito.Random.String.CombinationOfLettersAndNumbers(10);
+        TestContext.Out.WriteLine(randomAlpha);
+        Assert.That(randomAlpha, Has.Length.EqualTo(10));
+    }
+
+    [Test]
+    public void TestComplex()
+    {
+        var randomAlpha = Mojito.Random.String.Complex(10);
+        TestContext.Out.WriteLine(randomAlpha);
+        Assert.That(randomAlpha, Has.Length.EqualTo(10));
+    }
+
+    [Test]
+    public void TestCreate()
+    {
+        var result = Mojito.Random.String.Create("123abc", 10);
+        TestContext.Out.WriteLine(result.GetOk());
+        Assert.That(result.Success, Is.True);
+    }
+}
