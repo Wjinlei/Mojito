f86ea6a2f144e0dac0ea9e8b987eddd96d12aa14
Wjinlei <freebufer.wang@gmail.com>
2023-02-24 14:48:59 +0800

    test: Add NumberTest class.

---
+++ TestMojito/Random/Number.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+
+namespace TestMojito.Random;
+
+public class Number
+{
+    [Test]
+    public void TestCreate()
+    {
+        var value = Mojito.Random.Number.Create(0, 10);
+        var expectedMember = new ArrayList { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        Assert.That(expectedMember, Has.Member(value));
+    }
+}
