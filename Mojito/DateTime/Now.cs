4529b78cbfb578a63785daf9ed828ed4a79a3644
Wjinlei <freebufer.wang@gmail.com>
2023-02-24 11:44:17 +0800

    feat: Add DateTime.Now class.

---
+++ Mojito/DateTime/Now.cs
@@ -0,0 +1,95 @@
+﻿namespace Mojito.DateTime;
+
+public static class Now
+{
+    /// <summary>
+    /// Gets a string representation of the number of hours in the current time
+    /// </summary>
+    /// <returns>Returns a time string in HH format</returns>
+    public static string GetHour()
+    {
+        return System.DateTime.Now.ToString("HH");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the number of minutes in the current time
+    /// </summary>
+    /// <returns>Return a time string in mm format</returns>
+    public static string GetMinute()
+    {
+        return System.DateTime.Now.ToString("mm");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the number of seconds of the current time
+    /// </summary>
+    /// <returns>Returns a time string in ss format</returns>
+    public static string GetSecond()
+    {
+        return System.DateTime.Now.ToString("ss");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the current time
+    /// </summary>
+    /// <returns>The time string is in HH:mm:ss format</returns>
+    public static string GetTime()
+    {
+        return System.DateTime.Now.ToString("HH:mm:ss");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the year of the current time
+    /// </summary>
+    /// <returns>Returns a time string in yyyy format</returns>
+    public static string GetYear()
+    {
+        return System.DateTime.Now.ToString("yyyy");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the month of the current time
+    /// </summary>
+    /// <returns>Return a time string in MM format</returns>
+    public static string GetMonth()
+    {
+        return System.DateTime.Now.ToString("MM");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the number of days in the current time
+    /// </summary>
+    /// <returns>Return a time string in dd format</returns>
+    public static string GetDay()
+    {
+        return System.DateTime.Now.ToString("dd");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the current date
+    /// </summary>
+    /// <returns>The time string is in the format yyyy-MM-dd</returns>
+    public static string GetDate()
+    {
+        return System.DateTime.Now.ToString("yyyy-MM-dd");
+    }
+
+    /// <summary>
+    /// Gets a string representation of the current date and time
+    /// </summary>
+    /// <returns>The value is a time string in the format yyyy-MM-dd HH:mm:ss</returns>
+    public static string GetDateTime()
+    {
+        return System.DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+    }
+
+    /// <summary>
+    /// Returns the current time string formatted.
+    /// </summary>
+    /// <param name="timeFormat">Time format</param>
+    /// <returns></returns>
+    public static string Format(string timeFormat)
+    {
+        return System.DateTime.Now.ToString(timeFormat);
+    }
+}
