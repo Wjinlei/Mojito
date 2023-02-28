aeea23b0d9d3690fffda1b54b0b4b1121d202e2b
Wjinlei <freebufer.wang@gmail.com>
2023-02-28 12:22:05 +0800

    feat: Add Cmd class.

---
+++ Mojito/Cmd.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics;
+
+namespace Mojito;
+
+public static class Cmd
+{
+    /// <summary>
+    /// Execute a command
+    /// </summary>
+    /// <param name="cmd">Command text</param>
+    /// <returns></returns>
+    public static Result<string> Execute(string cmd)
+    {
+        var compiler = new Process();
+
+        switch (Environment.OSVersion.Platform)
+        {
+            case PlatformID.Win32NT:
+                compiler.StartInfo.FileName = "cmd.exe";
+                break;
+            case PlatformID.Unix:
+                compiler.StartInfo.FileName = "/bin/bash";
+                break;
+            default:
+                return Result<string>.Error(new Exception("Unsupported operating system."));
+        }
+
+        compiler.StartInfo.RedirectStandardInput = true;
+        compiler.StartInfo.RedirectStandardOutput = true;
+        compiler.StartInfo.RedirectStandardError = true;
+        compiler.StartInfo.CreateNoWindow = true;
+
+        try
+        {
+            compiler.StartInfo.UseShellExecute = false; // UWP is set to true PlatformNotSupportedException will happen
+            compiler.Start();
+            compiler.StandardInput.WriteLine(cmd + "&exit");
+            compiler.StandardInput.AutoFlush = true;
+
+            var output = compiler.StandardOutput.ReadToEnd();
+            var error = compiler.StandardError.ReadToEnd();
+
+            compiler.WaitForExit();
+            if (error != "")
+                return Result<string>.Error(new Exception(error));
+            return Result<string>.Ok(output);
+        }
+        catch (Exception ex)
+        {
+            return Result<string>.Error(ex);
+        }
+    }
+}
