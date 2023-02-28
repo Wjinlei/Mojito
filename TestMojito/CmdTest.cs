0282c705c265338af614ff6f7b418eb331d1fecf
Wjinlei <freebufer.wang@gmail.com>
2023-02-28 12:22:14 +0800

    test: Add CmdTest class.

---
+++ TestMojito/CmdTest.cs
@@ -0,0 +1,15 @@
+﻿namespace TestMojito;
+
+public class CmdTest
+{
+    [Test]
+    public void TestExec()
+    {
+        var result = Mojito.Cmd.Execute(@"netstat -ano");
+        if (!result.Success)
+            TestContext.Out.WriteLine(result.Message);
+
+        Assert.That(result.Success, Is.True);
+        TestContext.Out.WriteLine(result.GetOk());
+    }
+}
