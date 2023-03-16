fe993129e8c3c387d61c91413829d9b716533c67
Wjinlei <freebufer.wang@gmail.com>
2023-03-16 15:20:14 +0800

    refactor: Rename param.

--- Mojito/Validator.cs
+++ Mojito/Validator.cs
@@ -4,12 +4,12 @@ namespace Mojito;
 
 public static class Validator
 {
-    public static bool IsEmail(string url)
+    public static bool IsEmail(string email)
     {
         var regex = new Regex(
             @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
             RegexOptions.None, TimeSpan.FromMilliseconds(150));
 
-        return regex.Match(url).Success;
+        return regex.Match(email).Success;
     }
 }
