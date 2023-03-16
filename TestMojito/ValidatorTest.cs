06177aa06326731087d846ffd7a1b4ce48d311e7
Wjinlei <freebufer.wang@gmail.com>
2023-03-16 14:20:47 +0800

    test: Add ValidatorTest class.

---
+++ TestMojito/ValidatorTest.cs
@@ -0,0 +1,11 @@
+﻿namespace TestMojito;
+
+public class ValidatorTest
+{
+    [Test]
+    public void TestIsEmail()
+    {
+        var ok = Mojito.Validator.IsEmail("service@adm.net");
+        Assert.That(ok, Is.True);
+    }
+}
