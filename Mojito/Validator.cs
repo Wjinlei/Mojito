fd7c9ebe0f68336f524c6e12d2b826ccd4fbaef8
Wjinlei <freebufer.wang@gmail.com>
2023-03-16 16:06:16 +0800

    feat: Add IsLetter in Validator.

--- Mojito/Validator.cs
+++ Mojito/Validator.cs
@@ -12,4 +12,39 @@ public static class Validator
 
         return regex.Match(email).Success;
     }
+
+    /// <summary>
+    /// Checks whether a given string is a pure letter
+    /// </summary>
+    /// <param name="letter">String</param>
+    /// <returns></returns>
+    public static bool IsLetter(string letter)
+    {
+        var regex = new Regex(
+            @"^[a-zA-Z]+$",
+            RegexOptions.None, TimeSpan.FromMilliseconds(150));
+
+        return regex.Match(letter).Success;
+    }
+
+    /// <summary>
+    /// Checks whether a given string is a pure letter of the specified length
+    /// </summary>
+    /// <param name="letter">String</param>
+    /// <param name="min">Min length</param>
+    /// <param name="max">Max length</param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentOutOfRangeException"></exception>
+    public static bool IsLetter(string letter, uint min, uint max)
+    {
+        if (min == 0)
+            throw new ArgumentOutOfRangeException("min cannot equal 0");
+        if (min > max)
+            throw new ArgumentOutOfRangeException("min cannot be greater than max");
+
+        var re = "^[a-zA-Z]{" + min + "," + max + "}$";
+        var regex = new Regex(re, RegexOptions.None, TimeSpan.FromMilliseconds(150));
+
+        return regex.Match(letter).Success;
+    }
 }
