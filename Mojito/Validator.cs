3102d29367008827bab91ae7bf2ae8f2682c20dc
Wjinlei <freebufer.wang@gmail.com>
2023-03-16 14:20:33 +0800

    feat: Add Validator class.

---
+++ Mojito/Validator.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Mojito;
+
+public static class Validator
+{
+    public static bool IsEmail(string url)
+    {
+        var regex = new Regex(
+            @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$",
+            RegexOptions.None, TimeSpan.FromMilliseconds(150));
+
+        return regex.Match(url).Success;
+    }
+}
