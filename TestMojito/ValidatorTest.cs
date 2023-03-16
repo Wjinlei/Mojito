1e45ff7cc69242ada8265ea0f8a086693054713d
Wjinlei <freebufer.wang@gmail.com>
2023-03-16 16:06:35 +0800

    test: Add TestIsLetter in ValidatorTest.

--- TestMojito/ValidatorTest.cs
+++ TestMojito/ValidatorTest.cs
@@ -8,4 +8,16 @@ public class ValidatorTest
         var ok = Mojito.Validator.IsEmail("service@adm.net");
         Assert.That(ok, Is.True);
     }
+
+    [Test]
+    public void TestIsLetter()
+    {
+        var ok1 = Mojito.Validator.IsLetter("example");
+        var ok2 = Mojito.Validator.IsLetter("example", 1, 10);
+        Assert.Multiple(() =>
+        {
+            Assert.That(ok1, Is.True);
+            Assert.That(ok2, Is.True);
+        });
+    }
 }
