a24086483b291ab0c3e0bff7b56bc26b9927240c
Wjinlei <freebufer.wang@gmail.com>
2023-03-17 15:20:51 +0800

    refactor: Optimize ValidatorTest.

--- Mojito.Test/ValidatorTest.cs
+++ Mojito.Test/ValidatorTest.cs
@@ -10,67 +10,184 @@ public class ValidatorTest
     }
 
     [Test]
-    public void TestIsLetter()
+    public void TestIsLetter1()
     {
-        var ok1 = Validator.IsLetter("example");
-        var ok2 = Validator.IsLetter("example", 1, 10);
-        Assert.Multiple(() =>
+        var strMap = new Dictionary<string, bool>
         {
-            Assert.That(ok1, Is.True);
-            Assert.That(ok2, Is.True);
-        });
+            { "123123", false },
+            { "123abc", false },
+            { "abc123", false },
+            { "!@#$%^&*=", false },
+            { "abc123!@#", false },
+            { "!@#abc123", false },
+            { "123!@#abc", false },
+            { "abcabc", true },
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsLetter(str.Key);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
     }
 
     [Test]
-    public void TestIsDigit()
+    public void TestIsLetter2()
     {
-        var ok1 = Validator.IsDigit("123");
-        var ok2 = Validator.IsDigit("123", 1, 10);
-        Assert.Multiple(() =>
+        var strMap = new Dictionary<string, bool>
         {
-            Assert.That(ok1, Is.True);
-            Assert.That(ok2, Is.True);
-        });
+            { "123123551", false },
+            { "123abc123", false },
+            { "abc123abc", false },
+            { "!@#$%^&*=", false },
+            { "abc123!@#", false },
+            { "!@#abc123", false },
+            { "123!@#abc", false },
+            { "abcabcabc", true },
+            { "abcabcabcabc", false },
+            { "abc", false },
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsLetter(str.Key, 8, 10);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
     }
 
     [Test]
-    public void TestIsAlpha()
+    public void TestIsDigit1()
     {
-        var ok1 = Validator.IsAlpha("123");
-        var ok2 = Validator.IsAlpha("abc");
-        var ok3 = Validator.IsAlpha("abc123");
-        var ok4 = Validator.IsAlpha("abc123", 1, 10);
-        Assert.Multiple(() =>
+        var strMap = new Dictionary<string, bool>
         {
-            Assert.That(ok1, Is.False);
-            Assert.That(ok2, Is.False);
-            Assert.That(ok3, Is.True);
-            Assert.That(ok4, Is.True);
-        });
+            { "123abc", false },
+            { "abc123", false },
+            { "abcabc", false },
+            { "!@#$%^&*=", false },
+            { "abc123!@#", false },
+            { "!@#abc123", false },
+            { "123!@#abc", false },
+            { "123123", true },
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsDigit(str.Key);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
     }
 
     [Test]
-    public void TestIsComplex()
+    public void TestIsDigit2()
     {
-        var ok1 = Validator.IsComplex("123");
-        var ok2 = Validator.IsComplex("abc");
-        var ok3 = Validator.IsComplex("abc123");
-        var ok4 = Validator.IsComplex("123sdf");
-        var ok5 = Validator.IsComplex("123sdf!@#");
-        var ok6 = Validator.IsComplex("!@3123sdf");
-        var ok7 = Validator.IsComplex("gsd!@3123");
-        var ok8 = Validator.IsComplex("321!@31a", 1, 10);
-        Assert.Multiple(() =>
+        var strMap = new Dictionary<string, bool>
         {
-            Assert.That(ok1, Is.False);
-            Assert.That(ok2, Is.False);
-            Assert.That(ok3, Is.False);
-            Assert.That(ok4, Is.False);
-            Assert.That(ok5, Is.True);
-            Assert.That(ok6, Is.True);
-            Assert.That(ok7, Is.True);
-            Assert.That(ok8, Is.True);
-        });
+            { "123abc123", false },
+            { "abc123abc", false },
+            { "abcabcabc", false },
+            { "!@#$%^&*=", false },
+            { "abc123!@#", false },
+            { "!@#abc123", false },
+            { "123!@#abc", false },
+            { "123123551", true },
+            { "111223123551", false },
+            { "123", false },
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsDigit(str.Key, 8, 10);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
+    }
+
+    [Test]
+    public void TestIsAlpha1()
+    {
+        var strMap = new Dictionary<string, bool>
+        {
+            { "123123", false },
+            { "abcabc", false },
+            { "abc123!@#", false },
+            { "!@#abc123", false },
+            { "123!@#abc", false },
+            { "123abc", true },
+            { "abc123", true },
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsAlpha(str.Key);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
+    }
+
+    [Test]
+    public void TestIsAlpha2()
+    {
+        var strMap = new Dictionary<string, bool>
+        {
+            { "123123123", false },
+            { "abcabcabc", false },
+            { "abc123!@#", false },
+            { "!@#abc123", false },
+            { "123!@#abc", false },
+            { "123abc123", true },
+            { "abc123abc", true },
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsAlpha(str.Key, 8, 10);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
+    }
+
+    [Test]
+    public void TestIsComplex1()
+    {
+        var strMap = new Dictionary<string, bool>
+        {
+            { "123123123", false },
+            { "abcabcabc", false },
+            { "abc123abc", false },
+            { "123abc123", false },
+            { "!@#$%^&*:", false },
+            { "abc123@#!", true },
+            { "123abc@#!", true },
+            { "@#!abc123", true },
+            { "@#!123abc", true }
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsComplex(str.Key);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
+    }
+
+    [Test]
+    public void TestIsComplex2()
+    {
+        var strMap = new Dictionary<string, bool>
+        {
+            { "abcabcabc", false },
+            { "123123123", false },
+            { "abc123abc", false },
+            { "123abc123", false },
+            { "!@#$%^&*:", false },
+            { "abc123@#!", true },
+            { "123abc@#!", true },
+            { "@#!abc123", true },
+            { "abc123@#!()", false },
+            { "a13@", false },
+        };
+
+        foreach (var str in strMap)
+        {
+            var ok = Validator.IsComplex(str.Key, 8, 10);
+            Assert.That(ok, Is.EqualTo(str.Value));
+        }
     }
 
     [Test]
