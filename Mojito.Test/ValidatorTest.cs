c66e05f619e1591d9da6bedbabad97e344e19224
Wjinlei <freebufer.wang@gmail.com>
2023-03-17 11:12:02 +0800

    refactor: Optimize ValidatorTest.

--- Mojito.Test/ValidatorTest.cs
+++ Mojito.Test/ValidatorTest.cs
@@ -22,7 +22,7 @@ public class ValidatorTest
     }
 
     [Test]
-    public void TestIsDight()
+    public void TestIsDigit()
     {
         var ok1 = Validator.IsDigit("123");
         var ok2 = Validator.IsDigit("123", 1, 10);
@@ -36,24 +36,40 @@ public class ValidatorTest
     [Test]
     public void TestIsAlpha()
     {
-        var ok1 = Validator.IsAlpha("abc123");
-        var ok2 = Validator.IsAlpha("abc123", 1, 10);
+        var ok1 = Validator.IsAlpha("123");
+        var ok2 = Validator.IsAlpha("abc");
+        var ok3 = Validator.IsAlpha("abc123");
+        var ok4 = Validator.IsAlpha("abc123", 1, 10);
         Assert.Multiple(() =>
         {
-            Assert.That(ok1, Is.True);
-            Assert.That(ok2, Is.True);
+            Assert.That(ok1, Is.False);
+            Assert.That(ok2, Is.False);
+            Assert.That(ok3, Is.True);
+            Assert.That(ok4, Is.True);
         });
     }
 
     [Test]
     public void TestIsComplex()
     {
-        var ok1 = Validator.IsComplex("c#123\\");
-        var ok2 = Validator.IsComplex("c#123\\", 1, 10);
+        var ok1 = Validator.IsComplex("123");
+        var ok2 = Validator.IsComplex("abc");
+        var ok3 = Validator.IsComplex("abc123");
+        var ok4 = Validator.IsComplex("123sdf");
+        var ok5 = Validator.IsComplex("123sdf!@#");
+        var ok6 = Validator.IsComplex("!@3123sdf");
+        var ok7 = Validator.IsComplex("gsd!@3123");
+        var ok8 = Validator.IsComplex("321!@31a", 1, 10);
         Assert.Multiple(() =>
         {
-            Assert.That(ok1, Is.True);
-            Assert.That(ok2, Is.True);
+            Assert.That(ok1, Is.False);
+            Assert.That(ok2, Is.False);
+            Assert.That(ok3, Is.False);
+            Assert.That(ok4, Is.False);
+            Assert.That(ok5, Is.True);
+            Assert.That(ok6, Is.True);
+            Assert.That(ok7, Is.True);
+            Assert.That(ok8, Is.True);
         });
     }
 }
