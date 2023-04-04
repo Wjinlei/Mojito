101455f8c1579746bfc892af6cf4d97d2e17d702
Wjinlei <freebufer.wang@gmail.com>
2023-04-04 15:39:25 +0800

    feat: Add ToInt32OrDefault in Convert.

--- Mojito/Convert.cs
+++ Mojito/Convert.cs
@@ -109,4 +109,23 @@ public static class Convert
         }
         return byteArray;
     }
+
+
+    /// <summary>
+    /// Convert.ToInt32 or default value
+    /// </summary>
+    /// <param name="value">The value to be converted</param>
+    /// <param name="defaultValue">If the conversion fails, this default value is returned</param>
+    /// <returns></returns>
+    public static int ToInt32OrDefault(string value, int defaultValue)
+    {
+        try
+        {
+            return System.Convert.ToInt32(value);
+        }
+        catch (Exception)
+        {
+            return defaultValue;
+        }
+    }
 }
