811d383975e7809f85e69fedc42ec519cc9d888b
Wjinlei <freebufer.wang@gmail.com>
2023-05-06 10:50:53 +0800

    feat: Add ToInt16OrDefault and ToInt64OrDefault.

--- Mojito/Convert.cs
+++ Mojito/Convert.cs
@@ -110,6 +110,23 @@ public static class Convert
         return byteArray;
     }
 
+    /// <summary>
+    /// Convert.ToInt16 or default value
+    /// </summary>
+    /// <param name="value">The value to be converted</param>
+    /// <param name="defaultValue">If the conversion fails, this default value is returned</param>
+    /// <returns></returns>
+    public static short ToInt16OrDefault(string value, short defaultValue)
+    {
+        try
+        {
+            return System.Convert.ToInt16(value);
+        }
+        catch (Exception)
+        {
+            return defaultValue;
+        }
+    }
 
     /// <summary>
     /// Convert.ToInt32 or default value
@@ -122,6 +139,24 @@ public static class Convert
         try
         {
             return System.Convert.ToInt32(value);
+        }
+        catch (Exception)
+        {
+            return defaultValue;
+        }
+    }
+
+    /// <summary>
+    /// Convert.ToInt64 or default value
+    /// </summary>
+    /// <param name="value">The value to be converted</param>
+    /// <param name="defaultValue">If the conversion fails, this default value is returned</param>
+    /// <returns></returns>
+    public static long ToInt64OrDefault(string value, long defaultValue)
+    {
+        try
+        {
+            return System.Convert.ToInt64(value);
         }
         catch (Exception)
         {
