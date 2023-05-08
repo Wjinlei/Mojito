84ffade6c44a4605deb6b139d4817d55cc27b646
Wjinlei <freebufer.wang@gmail.com>
2023-05-08 14:33:04 +0800

    fix: Fixed bug.

--- Mojito.Test/ValidatorTest.cs
+++ Mojito.Test/ValidatorTest.cs
@@ -3,14 +3,7 @@
 public class ValidatorTest
 {
     [Test]
-    public void TestIsEmail()
-    {
-        var ok = Validator.IsEmail("service@adm.net");
-        Assert.That(ok, Is.True);
-    }
-
-    [Test]
-    public void TestIsLetter1()
+    public void TestIsLetter()
     {
         var strMap = new Dictionary<string, bool>
         {
@@ -32,31 +25,7 @@ public class ValidatorTest
     }
 
     [Test]
-    public void TestIsLetter2()
-    {
-        var strMap = new Dictionary<string, bool>
-        {
-            { "123123551", false },
-            { "123abc123", false },
-            { "abc123abc", false },
-            { "!@#$%^&*=", false },
-            { "abc123!@#", false },
-            { "!@#abc123", false },
-            { "123!@#abc", false },
-            { "abcabcabc", true },
-            { "abcabcabcabc", false },
-            { "abc", false },
-        };
-
-        foreach (var str in strMap)
-        {
-            var ok = Validator.IsLetter(str.Key, 8, 10);
-            Assert.That(ok, Is.EqualTo(str.Value));
-        }
-    }
-
-    [Test]
-    public void TestIsDigit1()
+    public void TestIsDigit()
     {
         var strMap = new Dictionary<string, bool>
         {
@@ -78,31 +47,7 @@ public class ValidatorTest
     }
 
     [Test]
-    public void TestIsDigit2()
-    {
-        var strMap = new Dictionary<string, bool>
-        {
-            { "123abc123", false },
-            { "abc123abc", false },
-            { "abcabcabc", false },
-            { "!@#$%^&*=", false },
-            { "abc123!@#", false },
-            { "!@#abc123", false },
-            { "123!@#abc", false },
-            { "123123551", true },
-            { "111223123551", false },
-            { "123", false },
-        };
-
-        foreach (var str in strMap)
-        {
-            var ok = Validator.IsDigit(str.Key, 8, 10);
-            Assert.That(ok, Is.EqualTo(str.Value));
-        }
-    }
-
-    [Test]
-    public void TestIsAlpha1()
+    public void TestIsAlpha()
     {
         var strMap = new Dictionary<string, bool>
         {
@@ -123,28 +68,7 @@ public class ValidatorTest
     }
 
     [Test]
-    public void TestIsAlpha2()
-    {
-        var strMap = new Dictionary<string, bool>
-        {
-            { "123123123", false },
-            { "abcabcabc", false },
-            { "abc123!@#", false },
-            { "!@#abc123", false },
-            { "123!@#abc", false },
-            { "123abc123", true },
-            { "abc123abc", true },
-        };
-
-        foreach (var str in strMap)
-        {
-            var ok = Validator.IsAlpha(str.Key, 8, 10);
-            Assert.That(ok, Is.EqualTo(str.Value));
-        }
-    }
-
-    [Test]
-    public void TestIsComplex1()
+    public void TestIsComplex()
     {
         var strMap = new Dictionary<string, bool>
         {
@@ -167,33 +91,9 @@ public class ValidatorTest
     }
 
     [Test]
-    public void TestIsComplex2()
+    public void TestIsUri()
     {
-        var strMap = new Dictionary<string, bool>
-        {
-            { "abcabcabc", false },
-            { "123123123", false },
-            { "abc123abc", false },
-            { "123abc123", false },
-            { "!@#$%^&*:", false },
-            { "abc123@#!", true },
-            { "123abc@#!", true },
-            { "@#!abc123", true },
-            { "abc123@#!()", false },
-            { "a13@", false },
-        };
-
-        foreach (var str in strMap)
-        {
-            var ok = Validator.IsComplex(str.Key, 8, 10);
-            Assert.That(ok, Is.EqualTo(str.Value));
-        }
-    }
-
-    [Test]
-    public void TestIsURL()
-    {
-        var urls = new[] {
+        var uris = new[] {
             "http://xn--fiqs8s.cn",
             "http://example.com",
             "http://qq.com",
@@ -204,28 +104,9 @@ public class ValidatorTest
             "https://example.com/admin/login.php",
         };
 
-        foreach (var url in urls)
+        foreach (var uri in uris)
         {
-            var ok = Validator.IsURL(url);
-            Assert.That(ok, Is.True);
-        }
-    }
-
-    [Test]
-    public void TestIsDomain()
-    {
-        var domains = new[] {
-            "xn--fiqs8s.cn",
-            "example.com",
-            "qq.com",
-            "example.test.cn",
-            "6.cn",
-            "test.co.net"
-        };
-
-        foreach (var domain in domains)
-        {
-            var ok = Validator.IsDomain(domain);
+            var ok = Validator.IsUri(uri);
             Assert.That(ok, Is.True);
         }
     }
@@ -269,5 +150,12 @@ public class ValidatorTest
             var ok = Validator.IsPhoneNumber(phoneNumber);
             Assert.That(ok, Is.True);
         }
+    }
+
+    [Test]
+    public void TestIsEmail()
+    {
+        var ok = Validator.IsEmail("service@adm.net");
+        Assert.That(ok, Is.True);
     }
 }
