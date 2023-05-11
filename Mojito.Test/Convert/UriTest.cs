3a918219aeb361559efd75c61e2dff4dad639020
Wjinlei <freebufer.wang@gmail.com>
2023-05-11 11:49:22 +0800

    refactor: Optimize Uri.

--- Mojito.Test/Convert/UriTest.cs
+++ Mojito.Test/Convert/UriTest.cs
@@ -6,15 +6,23 @@ public class UriTest
     public void TestToHost()
     {
         var uri = "https://www.example.com/admin/login.php?username=admin&password=admin123";
-        var host = Mojito.Convert.Uri.ToHost(uri);
-        Assert.That(host, Is.EqualTo("www.example.com"));
+        var result = Mojito.Convert.Uri.ToHost(uri);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.GetOk(), Is.EqualTo("www.example.com"));
+        });
     }
 
     [Test]
     public void TestToPort()
     {
         var uri = "https://www.example.com/admin/login.php?username=admin&password=admin123";
-        var port = Mojito.Convert.Uri.ToPort(uri);
-        Assert.That(port, Is.EqualTo(443));
+        var result = Mojito.Convert.Uri.ToPort(uri);
+        Assert.Multiple(() =>
+        {
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.GetOk(), Is.EqualTo("443"));
+        });
     }
 }
--- Mojito/Convert/Uri.cs
+++ Mojito/Convert/Uri.cs
@@ -2,24 +2,29 @@
 
 public static class Uri
 {
-    public static string ToHost(string uri)
-    {
-        var result = Parse(uri);
-        return result.Success ? result.GetOk().Host : "";
-    }
-
-    public static int ToPort(string uri)
-    {
-        var result = Parse(uri);
-        return result.Success ? result.GetOk().Port : -1;
-    }
-
     private static Result<System.Uri> Parse(string uri)
     {
-        bool isTrue = System.Uri.TryCreate(uri, UriKind.Absolute, out System.Uri? uriResult);
-        return
-            isTrue && uriResult != null
+        bool isTrue = System.Uri.TryCreate(
+            uri,
+            UriKind.Absolute,
+            out System.Uri? uriResult);
+
+        return isTrue && uriResult != null
             ? uriResult
             : new UriFormatException(nameof(uri));
+    }
+
+    public static Result<string> ToHost(string uri)
+    {
+        var result = Parse(uri);
+        return result.Success ? result.GetOk().Host : result.GetError();
+    }
+
+    public static Result<string> ToPort(string uri)
+    {
+        var result = Parse(uri);
+        return result.Success
+            ? result.GetOk().Port.ToString()
+            : result.GetError();
     }
 }
