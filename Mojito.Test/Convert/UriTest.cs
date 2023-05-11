59039d926d4578712f5a3ed843b1a7e1f6104870
Wjinlei <freebufer.wang@gmail.com>
2023-05-11 10:49:54 +0800

    refactor: Add ToPort in Uri.

--- Mojito.Test/Convert/UriTest.cs
+++ Mojito.Test/Convert/UriTest.cs
@@ -13,4 +13,12 @@ public class UriTest
             Assert.That(result.GetOk(), Is.EqualTo("www.example.com"));
         });
     }
+
+    [Test]
+    public void TestToPort()
+    {
+        var uri = "https://www.example.com/admin/login.php?username=admin&password=admin123";
+        var port = Mojito.Convert.Uri.ToPort(uri);
+        Assert.That(port, Is.EqualTo(443));
+    }
 }
--- Mojito/Convert/Uri.cs
+++ Mojito/Convert/Uri.cs
@@ -4,9 +4,23 @@ public static class Uri
 {
     public static Result<string> ToHost(string uri)
     {
-        bool isValidUrl = System.Uri.TryCreate(uri, UriKind.Absolute, out System.Uri? uriResult);
-        return isValidUrl && uriResult != null
-            ? uriResult.Host
+        var result = Parse(uri);
+        return result.Success ? result.GetOk().Host : result.GetError();
+    }
+
+    public static int ToPort(string uri)
+    {
+        var result = Parse(uri);
+        return result.Success ? result.GetOk().Port : -1;
+    }
+
+    private static Result<System.Uri> Parse(string uri)
+    {
+        bool isParseed = System.Uri.TryCreate(uri, UriKind.Absolute, out System.Uri? uriResult);
+
+        return
+            isParseed && uriResult != null
+            ? uriResult
             : new UriFormatException(nameof(uri));
     }
 }
