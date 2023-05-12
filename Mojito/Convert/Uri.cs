806f00e7fec251c2bac631f30097f58b7c705411
Wjinlei <freebufer.wang@gmail.com>
2023-05-12 14:32:24 +0800

    refactor: Optimize Uri.

--- Mojito/Convert/Uri.cs
+++ Mojito/Convert/Uri.cs
@@ -2,29 +2,39 @@
 
 public static class Uri
 {
-    private static Result<System.Uri> Parse(string uri)
+    /// <summary>
+    /// Parse the URI string and get the Port part
+    /// </summary>
+    /// <param name="uri">Uri string</param>
+    /// <returns></returns>
+    public static Result<string> ToPort(string uri)
     {
-        bool isTrue = System.Uri.TryCreate(
-            uri,
-            UriKind.Absolute,
-            out System.Uri? uriResult);
-
-        return isTrue && uriResult != null
-            ? uriResult
-            : new UriFormatException(nameof(uri));
+        var result = Parse(uri);
+        return result.Success ? result.GetOk().Port.ToString() : result.GetError();
     }
 
+    /// <summary>
+    /// Parse the URI string and get the Host part
+    /// </summary>
+    /// <param name="uri">Uri string</param>
+    /// <returns></returns>
     public static Result<string> ToHost(string uri)
     {
         var result = Parse(uri);
         return result.Success ? result.GetOk().Host : result.GetError();
     }
 
-    public static Result<string> ToPort(string uri)
+    /// <summary>
+    /// Parse URI string into a System.Uri object
+    /// </summary>
+    /// <param name="uri">Uri string</param>
+    /// <returns></returns>
+    private static Result<System.Uri> Parse(string uri)
     {
-        var result = Parse(uri);
-        return result.Success
-            ? result.GetOk().Port.ToString()
-            : result.GetError();
+        bool isTrue = System.Uri.TryCreate(
+            uri, UriKind.Absolute, out System.Uri? uriResult);
+
+        return isTrue && uriResult != null
+            ? uriResult : new UriFormatException(nameof(uri));
     }
 }
