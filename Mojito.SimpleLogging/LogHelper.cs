80f72ba8dc5ed41e82b840df07895d76079d5a15
Wjinlei <freebufer.wang@gmail.com>
2023-11-08 18:01:30 +0800

    refactor: Logger decorator

--- Mojito.SimpleLogging/LogHelper.cs
+++ Mojito.SimpleLogging/LogHelper.cs
@@ -4,13 +4,13 @@ namespace Mojito.SimpleLogging;
 
 public static class LogHelper
 {
-    private static readonly ConsoleLogger consoleLogger;
-    private static readonly FileLogger fileLogger;
+    private static readonly LoggerDecorator consoleLogger;
+    private static readonly LoggerDecorator fileLogger;
 
     static LogHelper()
     {
-        consoleLogger = new ConsoleLogger();
-        fileLogger = new FileLogger();
+        consoleLogger = new LoggerDecorator(new ConsoleLogger());
+        fileLogger = new LoggerDecorator(new FileLogger());
     }
 
     /// <summary>
@@ -26,47 +26,28 @@ public static class LogHelper
         };
     }
 
-    /// <summary>
-    /// 根据日志的pattern，重新生成消息
-    /// </summary>
-    /// <param name="level">日志级别</param>
-    /// <param name="message">原始消息</param>
-    /// <returns></returns>
-    private static string GetMessage(string level, string message)
-    {
-        return LogConfigHelper.GetLogPattern().Replace("%date", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"))
-            .Replace("%level", level)
-            .Replace("%message", message)
-            .Replace("%newline", Environment.NewLine);
-    }
-
     public static void Debug(string message)
     {
-        var newMessage = GetMessage("Debug", message);
-        GetLogger().Log(newMessage, LogLevel.Debug);
+        GetLogger().Log(message, LogLevel.Debug);
     }
 
     public static void Info(string message)
     {
-        var newMessage = GetMessage("Info", message);
-        GetLogger().Log(newMessage, LogLevel.Info);
+        GetLogger().Log(message, LogLevel.Info);
     }
 
     public static void Warn(string message)
     {
-        var newMessage = GetMessage("Warn", message);
-        GetLogger().Log(newMessage, LogLevel.Warn);
+        GetLogger().Log(message, LogLevel.Warn);
     }
 
     public static void Error(string message)
     {
-        var newMessage = GetMessage("Error", message);
-        GetLogger().Log(newMessage, LogLevel.Error);
+        GetLogger().Log(message, LogLevel.Error);
     }
 
     public static void Fatal(string message)
     {
-        var newMessage = GetMessage("Fatal", message);
-        GetLogger().Log(newMessage, LogLevel.Fatal);
+        GetLogger().Log(message, LogLevel.Fatal);
     }
 }
