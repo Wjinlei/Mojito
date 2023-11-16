3126bdc7c0e36a058e08feb8f7bd84371afd0dda
Wjinlei <freebufer.wang@gmail.com>
2023-11-16 11:06:19 +0800

    feat: add ConsoleLogger color

--- Mojito.SimpleLogging/Loggers/ConsoleLogger.cs
+++ Mojito.SimpleLogging/Loggers/ConsoleLogger.cs
@@ -4,6 +4,16 @@ public class ConsoleLogger : Logger
 {
     public override void Log(string message, LogLevel level)
     {
+        Console.ForegroundColor = level switch
+        {
+            LogLevel.Debug => ConsoleColor.DarkGreen,
+            LogLevel.Info => ConsoleColor.Gray,
+            LogLevel.Warn => ConsoleColor.DarkYellow,
+            LogLevel.Error => ConsoleColor.DarkRed,
+            LogLevel.Fatal => ConsoleColor.Magenta,
+            _ => ConsoleColor.White
+        };
+
         if (IsWriteable(level))
             Console.WriteLine(message);
     }
